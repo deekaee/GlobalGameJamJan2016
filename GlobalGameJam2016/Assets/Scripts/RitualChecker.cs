9df81dc650c4c0e42b1aa3ce0288654f0cedd1a5
Diane Watson <diane.watson@uwaterloo.ca>
2016-01-30 18:59:41 -0500

    stuff

--- GlobalGameJam2016/Assets/Scripts/RitualChecker.cs
+++ GlobalGameJam2016/Assets/Scripts/RitualChecker.cs
@@ -21,6 +21,18 @@ public class RitualChecker : MonoBehaviour {
 		foreach (RitualCircle r in RitualCircles) {
 			if (r.hasObject()) {
 				//Do All the things
+
+
+				///
+			
+			}
+
+			//accessing first shape
+			if (RitualCircles [0].hasObject ()) {
+				//if there is somethng in circle 1
+				if (RitualCircles [0].getObject ().GetComponent<RitualObjectBehaviour> ().shape == RitualObjectBehaviour.type.Circle)
+ {					//If that object is of type circle
+				}
 			}
 		}
 	}
