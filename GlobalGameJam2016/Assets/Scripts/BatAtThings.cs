237e1bb8b1f912e52bc6492d21dc02c20bb92c58
rinarene <rina.wehbe@gmail.com>
2016-01-30 13:25:53 -0500

    More particles

---
+++ GlobalGameJam2016/Assets/Scripts/BatAtThings.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatAtThings : MonoBehaviour {
+	public Vector3 force;
+	// Use this for initialization
+	void Start () {
+		UnityEngine.Cursor.visible = true;
+		Debug.logger.Log ("ARRRRRGGGG");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+	void OnMouseClick (){
+		Debug.logger.Log("In Mouse Down");
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100)) {
+			Debug.logger.Log ("In Hit");
+			if (hit.transform.gameObject.tag.Equals ("RitualObject")){
+				hit.transform.gameObject.GetComponent<Rigidbody>().AddForce (force);
+			}
+		}
+}
+}
---
+++ GlobalGameJam2016/Assets/Scripts/RitualChecker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualChecker : MonoBehaviour {
+	public RitualCircle[] RitualCircles;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckRitual ();
+	}
+
+	public void CheckRitual()
+	{
+		foreach (RitualCircle r in RitualCircles) {
+			if (r.hasObject()) {
+				//Do All the things
+			}
+		}
+	}
+}
