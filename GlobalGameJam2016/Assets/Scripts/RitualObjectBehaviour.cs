250cc1d4a7521440dbde16abb5aacc04b1b6d9a6
Diane Watson <diane.watson@uwaterloo.ca>
2016-01-30 12:04:47 -0500

    A bunch of things and code

---
+++ GlobalGameJam2016/Assets/Scripts/RitualObjectBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RitualObjectBehaviour : MonoBehaviour {
+	public string name;
+	private RitualCircle circle  = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setCircle(RitualCircle r)
+	{
+		circle = r;
+	}
+
+	public void leaveCircle()
+	{
+		circle = null;
+	}
+
+	public bool hasCircle()
+	{
+		return circle != null;
+	}
+
+	public RitualCircle getCircle()
+	{
+		return circle;
+	}
+
+	void OnMouseOver (){
+		if(Input.GetMouseButtonDown(0))
+			{
+			Vector3 v = GameObject.FindGameObjectWithTag ("Player").transform.forward;
+			Debug.Log ("VECTOR" + (v*100).ToString ());
+			this.gameObject.GetComponent<Rigidbody> ().AddExplosionForce (10, this.transform.position, 1);
+			}
+	}
+
+}
