9213fa6b31625452fa6ab21d81007408c8e10e6c
Diane Watson <diane.watson@uwaterloo.ca>
2016-01-31 13:22:09 -0500

    Many more things in rooms

---
+++ GlobalGameJam2016/Assets/Scripts/RandomMaterial.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMaterial : MonoBehaviour {
+	public Material[] materials;
+	// Use this for initialization
+	void Start () {
+		int index = Random.Range (0, materials.Length);
+		GetComponent<Renderer>().material = materials [index];
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
+
