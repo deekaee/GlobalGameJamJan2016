62e25b729a96cc13a7d385414d2e98ddecc6a24e
Diane Watson <diane.watson@uwaterloo.ca>
2016-01-31 13:50:37 -0500

    easier to do ritual

---
+++ GlobalGameJam2016/Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture myMovie;
+	public Texture2D Menu;
+	
+	void Start()
+	{
+		myMovie.Play();
+	}
+
+	void OnGUI()
+	{
+		
+		if (!myMovie.isPlaying) {
+			GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), Menu);
+		}
+		else
+			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), myMovie);
+	}
+}
